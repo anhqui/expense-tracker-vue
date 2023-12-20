08dc669cad5c3bb755a452c760d0c8a7d5876df5
Qui Nguyen <qui@gmail.com>
2023-12-20 13:32:48 -0500

    first commit

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,45 @@
+<template>
+    <h3>Add new transaction</h3>
+    <form id="form" @submit.prevent="onSubmit">
+        <div class="form-control">
+            <label for="text">Text</label>
+            <input type="text" id="text" v-model="text" placeholder="Enter text...">
+        </div>
+        <div class="form-control">
+            <label for="amount">Amount
+                (negative - expense, positive - income)
+            </label>
+            <input type="text" id="amount" v-model="amount" placeholder="Enter amount...">
+        </div>
+        <button class="btn">Add transaction</button>
+    </form>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+import {useToast} from 'vue-toastification';
+
+const text = ref("");
+const amount = ref("");
+
+const emit = defineEmits(['transactionSubmitted']);
+
+const toast = useToast();
+
+const onSubmit = () =>{
+    if(!text.value || !amount.value){
+        toast.error("Both fields must be filled");
+        return;
+    }
+
+    const transactionData = {
+        text: text.value,
+        amount: parseFloat(amount.value)
+    }
+    emit('transactionSubmitted', transactionData)
+
+//    console.log(text.value, amount.value); 
+   text.value = "";
+   amount.value = "";
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,4 @@
+<template>
+   <h2>Expense Tracker</h2>
+</template>
+
